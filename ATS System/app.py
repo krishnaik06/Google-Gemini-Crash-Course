da91f0ce0c28b0668632f70216bdab2566d7ad27
Tegveer Ghura <tegveerg@bu.edu>
2024-01-20 15:08:54 -0500

    Clean prompts on atsllm and ATS System

--- ATS System/app.py
+++ ATS System/app.py
@@ -2,12 +2,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 import base64
-import streamlit as st
-import os
 import io
-from PIL import Image 
-import pdf2image
+import os
+
 import google.generativeai as genai
+import pdf2image
+import streamlit as st
+from PIL import Image
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
@@ -40,10 +41,10 @@ def input_pdf_setup(uploaded_file):
 
 ## Streamlit App
 
-st.set_page_config(page_title="ATS Resume EXpert")
+st.set_page_config(page_title="ATS Resume Expert")
 st.header("ATS Tracking System")
 input_text=st.text_area("Job Description: ",key="input")
-uploaded_file=st.file_uploader("Upload your resume(PDF)...",type=["pdf"])
+uploaded_file=st.file_uploader("Upload your resume (PDF)...",type=["pdf"])
 
 
 if uploaded_file is not None:
@@ -57,13 +58,13 @@ submit1 = st.button("Tell Me About the Resume")
 submit3 = st.button("Percentage match")
 
 input_prompt1 = """
- You are an experienced Technical Human Resource Manager,your task is to review the provided resume against the job description. 
-  Please share your professional evaluation on whether the candidate's profile aligns with the role. 
- Highlight the strengths and weaknesses of the applicant in relation to the specified job requirements.
+You are an experienced Technical Human Resource Manager, your task is to review the provided resume against the job description.
+Please share your professional evaluation on whether the candidate's profile aligns with the role.
+Highlight the strengths and weaknesses of the applicant in relation to the specified job requirements.
 """
 
 input_prompt3 = """
-You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of data science and ATS functionality, 
+You are a skilled ATS (Applicant Tracking System) scanner with a deep understanding of data science and ATS functionality,
 your task is to evaluate the resume against the provided job description. give me the percentage of match if the resume matches
 the job description. First the output should come as percentage and then keywords missing and last final thoughts.
 """
@@ -75,7 +76,7 @@ if submit1:
         st.subheader("The Repsonse is")
         st.write(response)
     else:
-        st.write("Please uplaod the resume")
+        st.write("Please upload the resume")
 
 elif submit3:
     if uploaded_file is not None:
@@ -84,11 +85,11 @@ elif submit3:
         st.subheader("The Repsonse is")
         st.write(response)
     else:
-        st.write("Please uplaod the resume")
+        st.write("Please upload the resume")
 
 
 
-   
+
 
 
 
