4a7ad8b8ffae68dd0119fe38cad6c802d9bcbc51
Tegveer Ghura <tegveerg@bu.edu>
2024-02-07 16:51:17 -0500

    app ready for deployment

--- Food-Label-English-To-Hindi/app.py
+++ Food-Label-English-To-Hindi/app.py
@@ -1,7 +1,7 @@
 ### Food Label Translation from English to Hindi using Google Gemini Pro Vision API
 from dotenv import load_dotenv
 
-load_dotenv() ## load all the environment variables
+load_dotenv()  # load all the environment variables
 
 import os
 
@@ -11,40 +11,56 @@ from PIL import Image
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-## Function to load Google Gemini Pro Vision API And get response
-
-def get_gemini_response(prompt,image):
+# Function to load Google Gemini Pro Vision API And get response
+def get_gemini_response(prompt, image):
     model = genai.GenerativeModel('gemini-pro-vision')
-    if prompt!="":
-       response = model.generate_content([prompt,image])
+    if prompt != "":
+        response = model.generate_content([prompt, image])
     else:
-       response = model.generate_content(image)
+        response = model.generate_content(image)
     return response.text
 
-##initialize our streamlit app
+# Initialize Streamlit app
+st.set_page_config(page_title="Translate Food Package Labels From English to Hindi with Google Gemini",
+                   page_icon=":fork_and_knife:", layout="wide")
 
-st.set_page_config(page_title="Translate Food Package Labels From English to Hindi with Google Gemini")
+# Add food and health-related images
+fssai_image = "https://bsmedia.business-standard.com/_media/bs/img/article/2023-04/21/full/1682088340-8915.png?im=FeatureCrop,size=(826,465)"
+healthy_food_image = "https://img.freepik.com/free-photo/buddha-bowl-dish-with-vegetables-legumes-top-view_1150-42589.jpg"
+unhealthy_food_image = "https://t4.ftcdn.net/jpg/02/86/17/89/360_F_286178925_8zk89O9uC5JJVPvqhvBMUpaRxp8AFXzD.jpg"
 
+st.sidebar.image(fssai_image, use_column_width=True)
+st.sidebar.image(healthy_food_image, caption="Healthy Foods/स्वस्थ आहार", use_column_width=True)
+st.sidebar.image(unhealthy_food_image, caption="Unhealthy Foods/अस्वस्थ आहार", use_column_width=True)
+
+# Main content of the app
 st.title("Translate Food Package Labels From English to Hindi with Google Gemini")
 st.header("गूगल जेमिनी के साथ खाद्य पैकेज लेबल्स को अंग्रेजी से हिंदी में अनुवाद करें")
 
-uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
-image=""
+uploaded_file = st.file_uploader("Input food package label image/खाद्य पैकेज लेबल चित्र दर्ज करें", type=["jpg", "jpeg", "png"])
+image = ""
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image.", use_column_width=True)
+    st.image(image, caption="Your Uploaded Image ↑/आपकी डाली गई चित्र ↑", use_column_width=True)
 
-submit=st.button("Tell me the product details in Hindi")
+submit = st.button("Tell me the product details in Hindi/मुझे उत्पाद के विवरण हिंदी में बताएं")
 
-input_prompt="""
+input_prompt = """
 You are an expert nutritionist as well as an expert translator between English and Hindi.
-You need to read the food items (such as ingredients and nutrition) from the image of back packaging food label (mainly in English) and translate the product details to Hindi.
-First provide the translation in Hindi of all the following: ingredients, then the nutrition details of the food items, and lastly your verdict on the healthiness of the food product.
+You need to read the food items, including ingredients and nutritional facts, from the image of back packaging food label (mainly in English)
+and translate the product details to Hindi.
+
+Provide the translation in Hindi of all the following as separate titles: ingredients (and list them as numbered points in Hindi),
+then the nutrition details of the food items (and list them as numbered points in Hindi), then your verdict on the healthiness of the food product,
+and lastly what are some home cooked healthier alternatives to the product provided.
 """
 
-## If submit button is clicked
-
+# If submit button is clicked
 if submit:
-    response=get_gemini_response(input_prompt, image)
-    st.subheader("The Product Details in Hindi are:")
+    response = get_gemini_response(input_prompt, image)
+    st.subheader("The Product Details in Hindi are:/उत्पाद के विवरण हिंदी में:")
     st.write(response)
+
+    # Display the additional image below the product details
+    additional_image_url = "https://healthylife.werindia.com/wp-content/uploads/2015/04/Food-Safety-Label.jpg"
+    st.image(additional_image_url, caption="FSSAI's Food Label requirements in India/भारत में खाद्य सुरक्षा और मानक प्राधिकृति निगम के खाद्य लेबल आवश्यकताओं", use_column_width=False)
